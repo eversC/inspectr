b8ad14c5b913ff3a27e5f0bc1adf61ea260af7ab
chrise <chris.every@paxport.net>
2017-11-11 20:35:04 +0000

    more tests...

--- inspectr_test.go
+++ inspectr_test.go
@@ -71,6 +71,8 @@ var cappedslackstrings = []struct {
 	cappedstring string
 }{
 	{[]string{"v0.0.1"}, "v0.0.1"},
+	{[]string{"v0.0.1", "v0.0.2", "v0.0.3", "v0.0.4", "v0.0.5", "v0.0.6"},
+		"v0.0.1, v0.0.2, v0.0.3, v0.0.4, v0.0.5 + 1 more"},
 }
 
 func TestCappedSlackString(t *testing.T) {
@@ -79,6 +81,118 @@ func TestCappedSlackString(t *testing.T) {
 			cappedslackstring.cappedstring {
 			t.Errorf("cappedSlackString(%v) returned %s, expected %s",
 				cappedslackstring.candidates, v, cappedslackstring.cappedstring)
+		}
+	}
+}
+
+var imageURIs = []struct {
+	imageURI string
+	image    string
+}{
+	{"eversc/inspectr:v0.0.1-alpha", "eversc/inspectr"},
+}
+
+func TestImageFromURI(t *testing.T) {
+	for _, imageURI := range imageURIs {
+		if v := imageFromURI(imageURI.imageURI); v != imageURI.image {
+			t.Errorf("imageFromURI(%s) returned %s, expected %s", imageURI.imageURI,
+				v, imageURI.image)
+		}
+	}
+}
+
+var versions = []struct {
+	splitImageStrings []string
+	version           string
+}{
+	{[]string{"eversc/inspectr", "v0.0.1-alpha"}, "v0.0.1-alpha"},
+}
+
+func TestVersionFromURI(t *testing.T) {
+	for _, version := range versions {
+		if v := versionFromURI(version.splitImageStrings); v != version.version {
+			t.Errorf("versionFromURI(%v) returned %s, expected %s",
+				version.splitImageStrings, v, version.version)
+		}
+	}
+}
+
+var inspectrProjectMapKeys = []struct {
+	inspectrMapKey string
+	project        string
+}{
+	{"project:cluster:image:pod:container", "project"},
+}
+
+func TestProjectFromInspectrMapKey(t *testing.T) {
+	for _, inspectrMapKey := range inspectrProjectMapKeys {
+		if v := projectFromInspectrMapKey(inspectrMapKey.inspectrMapKey); v != inspectrMapKey.project {
+			t.Errorf("projectFromInspectrMapKey(%s) returned %s, expected %s",
+				inspectrMapKey.inspectrMapKey, v, inspectrMapKey.project)
+		}
+	}
+}
+
+var inspectrClusterMapKeys = []struct {
+	inspectrMapKey string
+	cluster        string
+}{
+	{"project:cluster:image:pod:container", "cluster"},
+}
+
+func TestClusterFromInspectrMapKey(t *testing.T) {
+	for _, inspectrMapKey := range inspectrClusterMapKeys {
+		if v := clusterFromInspectrMapKey(inspectrMapKey.inspectrMapKey); v != inspectrMapKey.cluster {
+			t.Errorf("clusterFromInspectrMapKey(%s) returned %s, expected %s",
+				inspectrMapKey.inspectrMapKey, v, inspectrMapKey.cluster)
+		}
+	}
+}
+
+var inspectrImageMapKeys = []struct {
+	inspectrMapKey string
+	image          string
+}{
+	{"project:cluster:image:pod:container", "image"},
+}
+
+func TestImageFromInspectrMapKey(t *testing.T) {
+	for _, inspectrMapKey := range inspectrImageMapKeys {
+		if v := imageFromInspectrMapKey(inspectrMapKey.inspectrMapKey); v != inspectrMapKey.image {
+			t.Errorf("imageFromInspectrMapKey(%s) returned %s, expected %s",
+				inspectrMapKey.inspectrMapKey, v, inspectrMapKey.image)
+		}
+	}
+}
+
+var inspectrPodMapKeys = []struct {
+	inspectrMapKey string
+	pod            string
+}{
+	{"project:cluster:image:pod:container", "pod"},
+}
+
+func TestPodFromInspectrMapKey(t *testing.T) {
+	for _, inspectrMapKey := range inspectrPodMapKeys {
+		if v := podFromInspectrMapKey(inspectrMapKey.inspectrMapKey); v != inspectrMapKey.pod {
+			t.Errorf("podFromInspectrMapKey(%s) returned %s, expected %s",
+				inspectrMapKey.inspectrMapKey, v, inspectrMapKey.pod)
+		}
+	}
+}
+
+var inspectrContainerMapKeys = []struct {
+	inspectrMapKey string
+	container      string
+}{
+	{"project:cluster:image:pod:container", "container"},
+}
+
+func TestContainerFromInspectrMapKey(t *testing.T) {
+	for _, inspectrMapKey := range inspectrContainerMapKeys {
+		if v := containerFromInspectrMapKey(inspectrMapKey.inspectrMapKey); v != inspectrMapKey.container {
+			t.Errorf("containerFromInspectrMapKey(%s) returned %s, expected %s",
+				inspectrMapKey.inspectrMapKey, v, inspectrMapKey.container)
 		}
 	}
 }
